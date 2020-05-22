1905b5ba278b73bdb96b1549d52fdd99f11f4b6d
tosh <kevinh939@gmail.com>
2020-05-21 23:10:35 -0500

    initial __manifest__

--- __manifest__.py
+++ __manifest__.py
@@ -1,35 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "sonderp_1",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'name': "sonderp",
+    'summary': """Demo o aplicación de prueba presentado por sonderp_ para su negocio""",
+    'description': """ Gestion de procesos de venta, compra, facturación e inventario para su negocio """,
+    'author': "Sonderp_ ",
+    'website': "http://www.sonderp.com",
+    'category': 'Operations',
+    'version': '13.0',
     'depends': ['base'],
-
-    # always loaded
     'data': [
+        # 'security/security.xml'
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/view_buy.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+        # 'demo/demo.xml',
     ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
